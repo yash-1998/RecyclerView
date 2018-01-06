29da933445a93aa95127a0a71ea3714a764efcf6
Yash <eternalblizzard1998@gmail.com>
2018-01-06 20:55:37 +0530

    Second Commit

--- app/src/main/java/com/example/yashwardhan/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/yashwardhan/materialdesign/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
             current.title=titles[i];
             data.add(current);
         }
+        //jhwbfejhbefgi
         return  data;
     }
 }
